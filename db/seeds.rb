973e20e539cf33c93d062a8faa9e951d1c317bf0
kalil1 <wkalil77@gmail.com>
2025-05-27 23:13:23 -0400

    update: updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,56 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+# Create Users
+users = []
+5.times do
+  users << User.create!(
+    email: Faker::Internet.unique.email,
+    password: 'password123',
+    provider: 'email',
+    uid: Faker::Internet.uuid
+  )
+end
+
+# Create Locations
+locations = []
+users.each do |user|
+  2.times do
+    locations << Location.create!(
+      nickname: Faker::Address.community,
+      city: Faker::Address.city,
+      street_address: Faker::Address.street_address,
+      state: Faker::Address.state_abbr,
+      zipcode: Faker::Address.zip_code,
+      business_name: Faker::Company.name,
+      user: user
+    )
+  end
+end
+
+# Create Clients
+clients = []
+users.each do |user|
+  3.times do
+    clients << Client.create!(
+      name: Faker::Name.name,
+      phone_number: Faker::PhoneNumber.phone_number,
+      email: Faker::Internet.email,
+      user: user
+    )
+  end
+end
+
+# Create Appointments
+10.times do
+  Appointment.create!(
+    appointment_time: Faker::Time.forward(days: 30, period: :morning),
+    duration: [30, 60, 90].sample,
+    price: rand(50.0..200.0).round(2),
+    location: locations.sample,
+    user: users.sample,
+    client: clients.sample
+  )
+end
+
+puts 'Seeding completed successfully!'
